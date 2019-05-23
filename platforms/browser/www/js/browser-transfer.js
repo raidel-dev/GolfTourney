5ef12b575900e8770ab0d20afef9edb06ec9f8a9
dalba1992 <dalba1992@yandex.com>
2019-05-23 17:57:53 +0800

    third

--- platforms/browser/www/js/browser-transfer.js
+++
@@ -1,5 +0,0 @@
-function viewLink(destination)
-{
-    //window.location.href = "browser.html?url="+destination;
-    var ref = cordova.InAppBrowser.open( destination, '_system', 'location=yes');
-}
